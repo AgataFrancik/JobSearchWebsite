b945c1584c3a526ba2d4a922ecbfd1be18ef13a8
bartekuznik <bartek.krecio@onet.pl>
2023-06-01 23:20:56 +0200

    IsAuth -> AllowAny

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -74,7 +74,7 @@ REST_AUTH ={
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
